5934fa51299ea0ab807e3cbe4a4a61e275c361f7
anna barykina <nyabarykina@yandex.ru>
2025-09-19 15:29:55 +0300

    код без документации

--- lab2.py
+++ lab2.py
@@ -1,7 +1,25 @@
-# key = 6
-# m = [1,2,3,6,8]
+def diap(d1,d2):
+    massive = []
+    for i in range(min(d1,d2), max(d1,d2)+1):
+        massive.append(i)
+    return massive
 
 def find(key, mass):
-    i = 0
-    while i < len(mass) and i != key:
-        print('deniska')
+    left, right = 0, len(mass)
+    guess = 0
+    while left < right:
+        mid = (right + left) // 2
+        if mass[mid] == key:
+            return mass[mid], guess
+        else:
+            guess += 1
+            if mass[mid] < key:
+                left = mid
+            else:
+                right = mid
+
+number = int(input('введите число '))
+dd1 = int(input('1 граница интервала '))
+dd2 = int(input('2 граница интервала '))
+diapp = diap(dd1,dd2)
+print(find(number, diapp))
